35aa25b3716c76e371b8cf8cc1ec8bfb730f5c96
Dan Bateman <dbateman0319@starkstate.net>
2018-01-11 21:39:18 -0500

    ejs added for templating

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+let bodyParser = require('body-parser');
 
 let app = express();
 
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// Routing
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	res.render('index');
 });
 
 app.listen(3000, function() {
-	console.log('App booted and listening on port 3000');
+	console.log('Server started on port 3000');
 });
