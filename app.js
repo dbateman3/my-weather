48365408d9552c6a9bae6b6178c3dedd227e1099
Dan Bateman <dbateman0319@starkstate.net>
2018-01-11 22:48:52 -0500

    Working form and validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-let bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
@@ -12,11 +13,48 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		let namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while(namespace.length) {
+			formParam += `[%{namespace.shift()}]`;
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+}));
+
+// Globals
+app.use(function(req, res, next) {
+	res.locals.errors = null;
+	next();
+});
 
 // Routing
 app.get('/', function(req, res) {
 	res.render('index');
 });
+
+app.post('/getweather', function(req, res) {
+	req.checkBody('address', 'address is required').notEmpty();
+	let errors = req.validationErrors();
+
+	if(errors){
+		console.log('Error');
+		res.render('index', {
+			errors: errors,
+		});
+	} else {
+		console.log('success');
+	}
+})
 
 app.listen(3000, function() {
 	console.log('Server started on port 3000');
